47ec2639aa67965b0dc2778539c5ad78f630b674
Yoginth S <yoginth@zoho.com>
2017-05-19 08:02:22 +0000

    Updated highlight across platform

--- resources/views/partials/highlight.blade.php
+++ resources/views/partials/highlight.blade.php
@@ -1,5 +1,5 @@
 
-<script src="{{ baseUrl('/libs/highlightjs/highlight.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/highlight.min.js"></script>
 <script>
     $(function() {
         var aCodes = document.getElementsByTagName('pre');
