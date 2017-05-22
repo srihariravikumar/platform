47af83f108f9c39a14c26d020594b78c756db26d
Yoginth S <yoginth@zoho.com>
2017-05-22 08:07:54 +0000

    Updated Rawgit in icons

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -16,10 +16,10 @@
 
     <link rel="stylesheet" href="{{ versioned_asset('css/styles.css') }}">
     <link rel="stylesheet" media="print" href="https://cdn.rawgit.com/doctub/static/master/css/print-styles.css?version=v.prod">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/doctub/static/master/css/icon-bundle.css">
     
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="https://cdn.rawgit.com/doctub/static/master/js/jquery.min.js"></script>
+    <script src="https://cdn.rawgit.com/doctub/static/master/js/jquery-ui.min.js"></script>
     <script src="{{ baseUrl('/translations') }}"></script>
 
     @yield('head')
