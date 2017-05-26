411836cc72c1e15df08dbab50a690a5927df9db0
Yoginth S <yoginth@zoho.com>
2017-05-26 14:31:51 +0000

    Updated Highlight to Google CND

--- resources/views/partials/highlight.blade.php
+++ resources/views/partials/highlight.blade.php
@@ -1,5 +1,5 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/highlight.min.js"></script>
+<script src="https://doctub-cdn.firebaseapp.com/js/highlight.min.js"></script>
 <script>
     $(function() {
         var aCodes = document.getElementsByTagName('pre');
