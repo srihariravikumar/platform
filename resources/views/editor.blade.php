8fa55ac65197d227c3cb3c21973e465c6d38f2d0
Yoginth S <yoginth@zoho.com>
2017-05-27 14:09:31 +0000

    Updated Gravatar for prefetches

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -9,7 +9,7 @@
     <meta name="theme-color" content="#F48024">
     <meta charset="utf-8">
     
-    <link rel="dns-prefetch" href="https://s.gravatar.com/">
+    <link rel="dns-prefetch" href="https://secure.gravatar.com/">
     <link rel="dns-prefetch" href="https://doctub-cdn.firebaseapp.com/">
     <link rel="dns-prefetch" href="https://chart.googleapis.com/">
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net/">
