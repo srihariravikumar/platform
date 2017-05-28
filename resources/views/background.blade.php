1b6ff314cd1d0f227269c5ff5d4dd3b14acfdd52
Yoginth S <yoginth@zoho.com>
2017-05-28 04:53:42 +0000

    Added Image(back) to Google CDN

--- resources/views/background.blade.php
+++ resources/views/background.blade.php
@@ -9,9 +9,7 @@
     <meta name="theme-color" content="#F48024">
     <meta charset="utf-8">
 
-    <link rel="dns-prefetch" href="https://secure.gravatar.com/">
     <link rel="dns-prefetch" href="https://doctub-cdn.firebaseapp.com/">
-    <link rel="dns-prefetch" href="https://chart.googleapis.com/">
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net/">
     
     <link rel="manifest" href="/manifest.json">
