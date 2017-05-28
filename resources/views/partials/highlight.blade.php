c10242887b561c28645f43cf2f397379be4706ca
Yoginth S <yoginth@zoho.com>
2017-05-28 13:57:57 +0000

    Changed highlight.js -> Codemirror

---
+++ resources/views/partials/highlight.blade.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="https://codemirror.net/lib/codemirror.css">
+<script src="https://codemirror.net/lib/codemirror.css"></script>
+<script>
+  var editor = CodeMirror.fromTextArea(myTextarea, {
+    lineNumbers: true
+  });
+</script>
