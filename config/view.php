82697570e7845d4efeb2254f2f2e5cf3bccca6ac
Yoginth <yoginth@zoho.com>
2017-05-29 19:19:05 +0530

    Removed comment in config/view.php

--- config/view.php
+++ config/view.php
@@ -12,17 +12,6 @@ return [
         realpath(base_path('resources/views')),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | This option determines where all the compiled Blade templates will be
-    | stored for your application. Typically, this is within the storage
-    | directory. However, as usual, you are free to change this value.
-    |
-    */
-
     'compiled' => realpath(storage_path('framework/views')),
 
 ];
