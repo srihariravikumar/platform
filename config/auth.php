a9bc2e63987091b4ae50711f9b7a0652a938c9b2
Yoginth <yoginth@zoho.com>
2017-05-30 18:18:57 +0530

    Added LICENSE in Comment

--- config/auth.php
+++ config/auth.php
@@ -1,9 +1,13 @@
 <?php
 
 /**
- * DocTub - Online Documentation Platform.
- * @author - Yoginth <yoginth@zoho.com>
+ * DocTub - Online Documentation/Wiki Platform.
  * Copyright (c) 2017-present, DocTub, Inc. All rights reserved.
+ *
+ * This source code is licensed under the BSD 3-Clause "New" or "Revised" License
+ * found in the LICENSE file in the root directory of this source tree. An additional
+ * grant of patent rights can be found in the PATENTS file in the same directory.
+ *
  */
 
 return [
