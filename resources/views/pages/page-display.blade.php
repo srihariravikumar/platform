5c0fab8da31dd3c3b811bd2d0282bb7f8544fef2
Yoginth S <yoginth@zoho.com>
2017-06-01 02:04:16 +0000

    Added Languages

--- resources/views/pages/page-display.blade.php
+++ resources/views/pages/page-display.blade.php
@@ -1,6 +1,11 @@
 <div ng-non-bindable>
 
-    <h1 id="bkmrk-page-title">{{$page->name}}</h1>
+    <h1 id="bkmrk-page-title">{{$page->name}}</h1> <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, multilanguagePage: true}, 'google_translate_element');
+}
+
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
     <div style="clear:left;"></div>
 
