68d975f41a2dfe9f0cd9e9dd7dfcf1d28bef26fb
Yoginth S <yoginth@zoho.com>
2017-06-02 16:58:22 +0000

    Updated Issue in Editor

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -23,29 +23,4 @@
     @include('components.image-manager', ['imageType' => 'gallery', 'uploaded_to' => $page->id])
     @include('components.entity-selector-popup')
 
-@stop@extends('editor')
-
-@section('head')
-    <script src="//unpkg.com/doctub@7.0.0/js/tinymce/tinymce.min.js"></script>
-@stop
-
-@section('body-class', 'flexbox')
-
-@section('content')
-
-    <div class="flex-fill flex">
-        <form action="{{ $page->getUrl() }}" autocomplete="off" data-page-id="{{ $page->id }}" method="POST" class="flex flex-fill">
-            @if(!isset($isDraft))
-                <input type="hidden" name="_method" value="PUT">
-            @endif
-            @include('pages/form', ['model' => $page])
-            @include('pages/form-toolbox')
-        </form>
-
-
-    </div>
-    
-    @include('components.image-manager', ['imageType' => 'gallery', 'uploaded_to' => $page->id])
-    @include('components.entity-selector-popup')
-
 @stop
