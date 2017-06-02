f1fecc7820dae4dab2b0e0837c52cc64f3a3532d
Yoginth S <yoginth@zoho.com>
2017-06-02 13:31:20 +0000

    Added TinyMCE 4.6.3

--- resources/views/pages/edit.blade.php
+++ resources/views/pages/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('editor')
 
 @section('head')
-    <script src="//unpkg.com/doctub@6.1.0/js/tinymce/tinymce.min.js"></script>
+    <script src="//unpkg.com/doctub@7.0.0/js/tinymce/tinymce.min.js"></script>
 @stop
 
 @section('body-class', 'flexbox')
