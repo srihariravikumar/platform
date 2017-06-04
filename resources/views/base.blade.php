520c0c5ac5ab0146c0cd4499d1c9fb119e822d63
Yoginth S <yoginth@zoho.com>
2017-06-04 11:28:56 +0000

    Changed CDN to UNPKG and its Tag Version

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -13,7 +13,7 @@
     <link rel="dns-prefetch" href="https://cdn.jsdelivr.net">
 
     <link rel="manifest" href="/manifest.json">
-    <link rel="icon" href="https://unpkg.com/doctub@{{ $version }}/images/favicon.png" type="image/x-icon"/>
+    <link rel="icon" href="https://unpkg.com/doctub@12.0.0/images/favicon.png" type="image/x-icon"/>
     <link rel="stylesheet" href="https://unpkg.com/doctub@12.0.0/css/core-bundle.css">
     <link rel="stylesheet" media="print" href="https://unpkg.com/doctub@12.0.0/css/core-bundle-1.css">
     <link rel="stylesheet" href="https://unpkg.com/doctub@12.0.0/css/fa-doctub.css">
