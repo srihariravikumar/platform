eb202121aceb50226822013147626a17e32cf27b
Yoginth S <yoginth@zoho.com>
2017-06-04 14:12:07 +0000

    Added Placeholder in Books Create

--- resources/views/books/form.blade.php
+++ resources/views/books/form.blade.php
@@ -1,13 +1,12 @@
 
 {{ csrf_field() }}
 <div class="form-group title-input">
-    <label for="name">{{ trans('common.name') }}</label>
-    @include('form/text', ['name' => 'name'])
+    @include('form/text', ['name' => 'name', 'placeholder' => "Name"])
 </div>
 
 <div class="form-group description-input">
     <label for="description">{{ trans('common.description') }}</label>
-    @include('form/textarea', ['name' => 'description'])
+    @include('form/textarea', ['name' => 'description', 'placeholder' => "escription"])
 </div>
 
 <div class="form-group">
