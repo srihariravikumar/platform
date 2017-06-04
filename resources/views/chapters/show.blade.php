0a89990eacf7a07c6e2733a8ab94e17052af3639
Yoginth S <yoginth@zoho.com>
2017-06-04 10:00:34 +0000

    Added Font Awesome

--- resources/views/chapters/show.blade.php
+++ resources/views/chapters/show.blade.php
@@ -12,7 +12,7 @@
                     <div class="action-buttons">
                         <span dropdown class="dropdown-container">
                             <iframe src="https://www.facebook.com/plugins/like.php?href={{ $book->getUrl('/') }}?&width=78&layout=button_count&action=like&size=small&show_faces=false&share=false&height=21&appId=1893355314012092" width="78" height="21" style="border:none;overflow:hidden;margin-bottom:-6px" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-                            <div dropdown-toggle class="text-button text-primary"><i class="zmdi zmdi-open-in-new"></i>{{ trans('entities.export') }}</div>
+                            <div dropdown-toggle class="text-button text-primary"><i class="fa fa-external-link"></i>{{ trans('entities.export') }}</div>
                             <ul class="wide">
                                 <li><a href="{{ $chapter->getUrl('/export/html') }}" target="_blank">{{ trans('entities.export_html') }} <span class="text-muted float right">.html</span></a></li>
                                 <li><a href="{{ $chapter->getUrl('/export/pdf') }}" target="_blank">{{ trans('entities.export_pdf') }} <span class="text-muted float right">.pdf</span></a></li>
@@ -20,23 +20,23 @@
                             </ul>
                         </span>
                         @if(userCan('page-create', $chapter))
-                            <a href="{{ $chapter->getUrl('/create-page') }}" class="text-pos text-button"><i class="zmdi zmdi-plus"></i>{{ trans('entities.pages_new') }}</a>
+                            <a href="{{ $chapter->getUrl('/create-page') }}" class="text-pos text-button"><i class="fa fa-plus"></i>{{ trans('entities.pages_new') }}</a>
                         @endif
                         @if(userCan('chapter-update', $chapter))
-                            <a href="{{ $chapter->getUrl('/edit') }}" class="text-primary text-button"><i class="zmdi zmdi-edit"></i>{{ trans('common.edit') }}</a>
+                            <a href="{{ $chapter->getUrl('/edit') }}" class="text-primary text-button"><i class="fa fa-pencil"></i>{{ trans('common.edit') }}</a>
                         @endif
                         @if(userCan('chapter-update', $chapter) || userCan('restrictions-manage', $chapter) || userCan('chapter-delete', $chapter))
                             <div dropdown class="dropdown-container">
-                                <a dropdown-toggle class="text-primary text-button"><i class="zmdi zmdi-more-vert"></i></a>
+                                <a dropdown-toggle class="text-primary text-button"><i class="fa fa-ellipsis-v"></i></a>
                                 <ul>
                                     @if(userCan('chapter-update', $chapter))
-                                        <li><a href="{{ $chapter->getUrl('/move') }}" class="text-primary"><i class="zmdi zmdi-folder"></i>{{ trans('common.move') }}</a></li>
+                                        <li><a href="{{ $chapter->getUrl('/move') }}" class="text-primary"><i class="fa fa-folder"></i>{{ trans('common.move') }}</a></li>
                                     @endif
                                     @if(userCan('restrictions-manage', $chapter))
-                                        <li><a href="{{ $chapter->getUrl('/permissions') }}" class="text-primary"><i class="zmdi zmdi-lock-outline"></i>{{ trans('entities.permissions') }}</a></li>
+                                        <li><a href="{{ $chapter->getUrl('/permissions') }}" class="text-primary"><i class="fa fa-lock"></i>{{ trans('entities.permissions') }}</a></li>
                                     @endif
                                     @if(userCan('chapter-delete', $chapter))
-                                        <li><a href="{{ $chapter->getUrl('/delete') }}" class="text-neg"><i class="zmdi zmdi-delete"></i>{{ trans('common.delete') }}</a></li>
+                                        <li><a href="{{ $chapter->getUrl('/delete') }}" class="text-neg"><i class="fa fa-trash"></i>{{ trans('common.delete') }}</a></li>
                                     @endif
                                 </ul>
                             </div>
@@ -68,13 +68,13 @@
                         <p class="text-muted">{{ trans('entities.chapters_empty') }}</p>
                         <p>
                             @if(userCan('page-create', $chapter))
-                                <a href="{{ $chapter->getUrl('/create-page') }}" class="text-page"><i class="zmdi zmdi-file-text"></i>{{ trans('entities.books_empty_create_page') }}</a>
+                                <a href="{{ $chapter->getUrl('/create-page') }}" class="text-page"><i class="fa fa-file-text"></i>{{ trans('entities.books_empty_create_page') }}</a>
                             @endif
                             @if(userCan('page-create', $chapter) && userCan('book-update', $book))
                                 &nbsp;&nbsp;<em class="text-muted">-{{ trans('entities.books_empty_or') }}-</em>&nbsp;&nbsp;&nbsp;
                             @endif
                             @if(userCan('book-update', $book))
-                                <a href="{{ $book->getUrl('/sort') }}" class="text-book"><i class="zmdi zmdi-book"></i>{{ trans('entities.books_empty_sort_current_book') }}</a>
+                                <a href="{{ $book->getUrl('/sort') }}" class="text-book"><i class="fa fa-book"></i>{{ trans('entities.books_empty_sort_current_book') }}</a>
                             @endif
                         </p>
                         <hr>
@@ -84,7 +84,7 @@
                 </div>
 
                 <div class="search-results" v-cloak v-if="searching">
-                    <h3 class="text-muted">{{ trans('entities.search_results') }} <a v-if="searching" v-on:click="clearSearch()" class="text-small"><i class="zmdi zmdi-close"></i>{{ trans('entities.search_clear') }}</a></h3>
+                    <h3 class="text-muted">{{ trans('entities.search_results') }} <a v-if="searching" v-on:click="clearSearch()" class="text-small"><i class="fa fa-times"></i>{{ trans('entities.search_clear') }}</a></h3>
                     <div v-if="!searchResults">
                         @include('partials/loading-icon')
                     </div>
@@ -99,18 +99,18 @@
                         @if($book->restricted)
                             <p class="text-muted">
                                 @if(userCan('restrictions-manage', $book))
-                                    <a href="{{ $book->getUrl('/permissions') }}"><i class="zmdi zmdi-lock-outline"></i>{{ trans('entities.books_permissions_active') }}</a>
+                                    <a href="{{ $book->getUrl('/permissions') }}"><i class="fa fa-lock"></i>{{ trans('entities.books_permissions_active') }}</a>
                                 @else
-                                    <i class="zmdi zmdi-lock-outline"></i>{{ trans('entities.books_permissions_active') }}
+                                    <i class="fa fa-lock"></i>{{ trans('entities.books_permissions_active') }}
                                 @endif
                             </p>
                         @endif
 
                         @if($chapter->restricted)
                             @if(userCan('restrictions-manage', $chapter))
-                                <a href="{{ $chapter->getUrl('/permissions') }}"><i class="zmdi zmdi-lock-outline"></i>{{ trans('entities.chapters_permissions_active') }}</a>
+                                <a href="{{ $chapter->getUrl('/permissions') }}"><i class="fa fa-lock"></i>{{ trans('entities.chapters_permissions_active') }}</a>
                             @else
-                                <i class="zmdi zmdi-lock-outline"></i>{{ trans('entities.chapters_permissions_active') }}
+                                <i class="fa fa-lock"></i>{{ trans('entities.chapters_permissions_active') }}
                             @endif
                         @endif
                     </div>
@@ -119,8 +119,8 @@
                 <div class="search-box">
                     <form v-on:submit="searchBook">
                         <input v-model="searchTerm" v-on:change="checkSearchForm()" type="text" name="term" placeholder="{{ trans('entities.chapters_search_this') }}">
-                        <button type="submit"><i class="zmdi zmdi-search"></i></button>
-                        <button v-if="searching" v-cloak class="text-neg" v-on:click="clearSearch()" type="button"><i class="zmdi zmdi-close"></i></button>
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                        <button v-if="searching" v-cloak class="text-neg" v-on:click="clearSearch()" type="button"><i class="fa fa-times"></i></button>
                     </form>
                 </div>
 
